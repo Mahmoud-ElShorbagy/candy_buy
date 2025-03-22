10b02361e0031509119da9aac9a297e4f330ca3d
MahmoudHmaza99 <mahmhamza199@gmail.com>
2025-03-22 06:12:08 +0200

    add if statement

--- lib/views/view_candy/favorites_page/view.dart
+++ lib/views/view_candy/favorites_page/view.dart
@@ -1,7 +1,15 @@
+import 'package:candy_buy/models/category_variables.dart';
+import 'package:candy_buy/views/view_candy/tiem/view.dart';
+import 'package:candy_buy/widgets/app/custom_categories.dart';
+import 'package:candy_buy/widgets/app/custom_content_category.dart';
+import 'package:candy_buy/widgets/app_text.dart';
 import 'package:flutter/material.dart';
 
 import '../../../core/helpers/app_colors.dart';
+import '../../../models/product_dto.dart';
 import '../header_nav/veiw.dart';
+
+List<CategoreytDTO> categoreyFavorit = [];
 
 class FavoritesPageView extends StatelessWidget {
   const FavoritesPageView({super.key});
@@ -16,10 +24,57 @@ class FavoritesPageView extends StatelessWidget {
           ),
           Expanded(
             child: ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return const Center(child: Text("asdad"));
-                }),
+              padding: EdgeInsets.all(16),
+              itemCount: categoreyFavorit.isEmpty ? 1 : categoreyFavorit.length,
+              itemBuilder: (context, index) {
+                if (categoreyFavorit.isEmpty) {
+                  return Center(
+                    child: Container(
+                      margin: EdgeInsets.only(top: 300),
+                      child: CustomText(
+                        text: "Favorites List Is Empty.",
+                        fontWeight: FontWeight.w700,
+                        color: AppColors.textBoldColor,
+                        fontSize: 16,
+                      ),
+                    ),
+                  );
+                } else {
+                  final categorey = categoreyFavorit[index];
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 12, right: 19),
+                    width: 373,
+                    height: 174,
+                    decoration: BoxDecoration(
+                      color: AppColors.backgroundWhiteColor,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Row(
+                      children: [
+                        CustomCategoriesView(
+                          images: categorey.images,
+                          width: categorey.width,
+                          height: categorey.height,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 40),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TimeView(time: time[index]),
+                              CustomContentCategoryView(
+                                title: categorey.title,
+                                content: categorey.content,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              },
+            ),
           ),
         ]));
   }
